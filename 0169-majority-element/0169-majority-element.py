4c88158589404fde4ff7dcf9fcdb6238e5b18aaa
AryanKumar05 <aryan.kumar111005@gmail.com>
2025-12-30 16:06:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (46.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,9 +4,17 @@ from typing import List
 class Solution:
 
     def majorityElement(self, nums: List[int]) -> int:
-        limit=math.floor(len(nums)/2)
-        obj=Counter(nums)
-        return obj.most_common(1)[0][0]
+    #     limit=math.floor(len(nums)/2)
+    #     obj=Counter(nums)
+    #     return obj.most_common(1)[0][0]
+        element=None
+        count=0
+        for num in nums:
+
+            if count==0:
+                element =num
+            count+=1 if element ==num else -1
+        return element
 
 
 
