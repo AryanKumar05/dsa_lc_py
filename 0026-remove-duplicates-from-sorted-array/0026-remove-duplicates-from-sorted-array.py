0dd83d91f90593161ea9a0b07f3206eba75e39da
AryanKumar05 <aryan.kumar111005@gmail.com>
2025-12-30 16:31:00 +0530

    Time: 6 ms (15.72%), Space: 18.4 MB (89.11%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,6 @@
+from collections import OrderedDict
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        nums[:]=list(OrderedDict.fromkeys(nums))
+        return len(nums)
+        
