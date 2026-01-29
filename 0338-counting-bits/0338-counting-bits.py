73142cb7542ef5292d87fd427fc26b695c209c3a
AryanKumar05 <aryan.kumar111005@gmail.com>
2026-01-30 04:01:59 +0530

    Time: 39 ms (23.49%), Space: 17.9 MB (14.62%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,19 @@
+def counter(i):
+    count=0
+    while i:
+        i&=i-1
+        count+=1
+    return count   
+    
+class Solution(object):
+        
+        
+    def countBits(self, n):
+            
+        idx=[]
+        for i in range(n+1):
+            bits=counter(i)
+            idx.append(bits)
+        return idx
+
+           
